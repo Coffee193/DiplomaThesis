d1f8411d574764678a3ce69b7d6b039d24e8ce2c
Chris Gertzos <chrisgertzos1@gmail.com>
2025-11-15 21:23:10 +0200

    Worked on Uploading Document to question box in frontend. Have to do backend

--- app/frontend/pages/ChatBox.jsx
+++ app/frontend/pages/ChatBox.jsx
@@ -9,16 +9,18 @@ export function ChatBox({ isloadingState, chatlist, chattype, convsetState, link
     const cbtextareaRef = useRef()
     const cbarrowRef = useRef()
     const navigate = useNavigate()
-    const [cbuState, cbusetState] = useState({'visible': false})
+    const [cbuState, cbusetState] = useState({'visible': false, 'isloading': true})
+    const cbinputRef = useRef()
+    const cbuploadRef = useRef()
 
     function CheckQuestion(){
-        if(cbtextareaRef.current.value.length === 0){
-            cbarrowRef.current.classList.add('cb_arrowdeactive')
-            cbarrowRef.current.classList.remove('cb_arrowactive')
-        }
-        else{
-            cbarrowRef.current.classList.add('cb_arrowactive')
-            cbarrowRef.current.classList.remove('cb_arrowdeactive')
+        if(cbuploadRef.current.value === ''){
+            if(cbtextareaRef.current.value.length === 0){
+                ArrowDeactive()
+            }
+            else{
+                ArrowActive()
+            }
         }
     }
 
@@ -60,8 +62,6 @@ export function ChatBox({ isloadingState, chatlist, chattype, convsetState, link
     }
 
     function SubmitQuestion(){
-        console.log(linkparams)
-        console.log('********')
         if(linkparams === undefined){
             CreateChat()
         }
@@ -106,6 +106,41 @@ export function ChatBox({ isloadingState, chatlist, chattype, convsetState, link
     }
 
     function UploadDocument(){
+        console.log('i was called')
+        cbusetState({'visible': true, 'name': cbinputRef.current.files[0]['name'], 'size': (cbinputRef.current.files[0]['size']/1024).toFixed(1), 'type': cbinputRef.current.files[0]['type'].split('/')[1].toUpperCase()})
+        let filereader = new FileReader();
+        filereader.readAsDataURL(cbinputRef.current.files[0])
+        filereader.onloadend = () => {
+            cbusetState(prevState => ({...prevState, 'isloading': false, 'data': filereader.result}))
+            UploadDeactive()
+            ArrowActive()
+        }
+    }
+
+    function ArrowActive(){
+        cbarrowRef.current.classList.add('cb_utilactive')
+        cbarrowRef.current.classList.remove('cb_utildeactive')
+    }
+    function ArrowDeactive(){
+        cbarrowRef.current.classList.add('cb_utildeactive')
+        cbarrowRef.current.classList.remove('cb_utilactive')
+    }
+    function UploadActive(){
+        cbuploadRef.current.classList.remove('cb_utildeactive')
+        cbuploadRef.current.classList.add('cb_utilactive', 'cb_upload')
+    }
+    function UploadDeactive(){
+        cbuploadRef.current.classList.remove('cb_upload', 'cb_utilactive')
+        cbuploadRef.current.classList.add('cb_utildeactive')
+    }
+    function EmptyTextArrowDeactive(){
+        if(cbtextareaRef.current.value === ''){
+            ArrowDeactive()
+        }
+    }
+
+    async function AskQuestionWithDocument(){
+        let response_status = null
         
     }
 
@@ -114,21 +149,22 @@ export function ChatBox({ isloadingState, chatlist, chattype, convsetState, link
             { isloadingState === false ? (
             <>
                 <div>
-                    <ChatBoxUpload cbuState={cbuState} cbusetState={cbusetState}/>
+                    <ChatBoxUpload cbuState={cbuState} cbusetState={cbusetState} cbinputRef={cbinputRef} UploadActive={UploadActive} EmptyTextArrowDeactive={EmptyTextArrowDeactive}/>
                     <textarea className='cb_textarea' placeholder='Ask Sapling' onChange={() => CheckQuestion()} ref={cbtextareaRef} onKeyDown={(event) => PressEnter(event)} autoFocus={true}/>
                 </div>
                 <div className='cb_infoholder'>
-                    <div className='cb_arrow cb_arrowactive cb_upload'>
+                    <div className='cb_util cb_utilactive cb_upload' onClick={() => cbinputRef.current.value === '' ? cbinputRef.current.click() : null} ref={cbuploadRef}>
                         <UploadFile/>
                         <div className={'cb_uploadtext ' + (chattype === 'main' ? 'cb_uploadmain' : 'cb_uploadbody')}>Upload File</div>
+                        <input type='file' className='cb_input' ref={cbinputRef} accept='text/xml' onChange={() => UploadDocument()}/>
                     </div>
-                    <div className='cb_arrow cb_arrowdeactive' onClick={() => SubmitQuestion()} ref={cbarrowRef}><ArrowUpload/></div>
+                    <div className='cb_util cb_utildeactive' onClick={() => SubmitQuestion()} ref={cbarrowRef}><ArrowUpload/></div>
                 </div>
                 <div className='cb_bg' onClick={() => cbtextareaRef.current.focus()}/>
                 </>
             ) : (
                 <div className='cb_loading'>
-                    <div className='cb_arrowload loading'/>
+                    <div className='cb_utilload loading'/>
                 </div>
             )
             }
--- app/frontend/pages/ChatBoxUpload.jsx
+++ app/frontend/pages/ChatBoxUpload.jsx
@@ -2,18 +2,23 @@ import '../styling/ChatBoxUpload.css'
 import { Document, XCloseIcon, BlocksLoad } from '../components/svgs/UtilIcons'
 import { useState } from 'react'
 
-export function ChatBoxUpload({ cbuState, cbusetState }){
+export function ChatBoxUpload({ cbuState, cbusetState, cbinputRef, UploadActive, EmptyTextArrowDeactive }){
 
-    const [isloadingState, isloadingsetState] = useState(true)
+    function CloseBoxUpload(){
+        cbusetState({'visible': false, 'isloading': true})
+        cbinputRef.current.value = ''
+        UploadActive()
+        EmptyTextArrowDeactive()
+    }
 
     return(
         <div className='cbu_holder' style={cbuState['visible'] === false ? {display: 'none'} : null}>
             <div className='cbu'>
-                {isloadingState === true ? '' : <div className='cbu_x'><XCloseIcon width={14} height={14}/></div>}
-                <div className='cbu_document'>{isloadingState === true ? <BlocksLoad width={30} height={30}/> : <Document width={30} height={30}/>}</div>
+                {cbuState['isloadingState'] === true ? '' : <div className='cbu_x' onClick={() => CloseBoxUpload()}><XCloseIcon width={14} height={14}/></div>}
+                <div className='cbu_document'>{cbuState['isloadingState'] === true ? <BlocksLoad width={30} height={30}/> : <Document width={30} height={30}/>}</div>
                 <div className='cbu_info'>
-                    <div className='cbu_top'>Name of the Doc is this</div>
-                    <div className='cbu_bottom'>XML 50kB</div>
+                    <div className='cbu_top'>{cbuState['name']}</div>
+                    <div className='cbu_bottom'>{cbuState['type']} {cbuState['size']}kB</div>
                 </div>
             </div>
         </div>
